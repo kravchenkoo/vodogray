d662e6be82898783d6bba3b483efd7ff8948693d
kravchenkoo <kravchenkoo14@gmail.com>
2025-03-28 21:36:59 +0200

    last preparations before deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'vodogray.media.strapiapp.site',
+      },
     ],
   },
 };
